4189194941103321d0c94c1817ddf1fffbb031d2
CPea <cpea2506@gmail.com>
2025-04-28 11:11:19 +0700

    Cleanup

--- src/greeter.cpp
+++ src/greeter.cpp
@@ -1,20 +1,20 @@
 #include <fmt/format.h>
-#include <greeter/greeter.h>
+#include <greeter.h>
 
 using namespace greeter;
 
 Greeter::Greeter(std::string _name) : name(std::move(_name)) {}
 
 std::string Greeter::greet(LanguageCode lang) const {
-  switch (lang) {
+    switch (lang) {
     default:
     case LanguageCode::EN:
-      return fmt::format("Hello, {}!", name);
+        return fmt::format("Hello, {}!", name);
     case LanguageCode::DE:
-      return fmt::format("Hallo {}!", name);
+        return fmt::format("Hallo {}!", name);
     case LanguageCode::ES:
-      return fmt::format("¡Hola {}!", name);
+        return fmt::format("¡Hola {}!", name);
     case LanguageCode::FR:
-      return fmt::format("Bonjour {}!", name);
-  }
+        return fmt::format("Bonjour {}!", name);
+    }
 }
